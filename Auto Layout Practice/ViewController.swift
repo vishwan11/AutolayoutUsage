33ba23c847d3716d37e05ef0a5711353b321c880
Viswanath Subramani S S <viswanathsubramaniss@gmail.com>
2017-12-09 22:40:21 +0530

    Complete Commit

---
+++ Auto Layout Practice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Auto Layout Practice
+//
+//  Created by Viswanath Subramani S S on 02/11/2017.
+//  Copyright © ViswanathSubramaniSS. All rights reserved.
+//	Assets provided by London App Brewery
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
